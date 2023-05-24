a0a805db0af4ab4b2765def5cf2378dbb2bcd9f4
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-05-24 13:17:16 +0300

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import HomeMoviesList from 'components/HomeMoviesList/HomeMoviesList';
+import { useState, useEffect } from 'react';
+import getTrendingMovies from 'services/apiTrendingMovies';
+import MovieList from 'components/MovieList/MovieList';
 
 const Home = () => {
-  return <HomeMoviesList />;
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const moviesData = await getTrendingMovies();
+      setMovies(moviesData);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Trending today</h1>
+      {movies ? <MovieList movies={movies} /> : <p>Loading...</p>}
+    </>
+  );
 };
 
 export default Home;
